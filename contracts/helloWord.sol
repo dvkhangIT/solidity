953c2d29a0b6e5b00b230bf30285e4eb67d07498
dvkhangIT <dvkhang2100321@student.ctuet.edu.vn>
2024-09-23 09:10:49 +0700

    visibility

--- contracts/helloWord.sol
+++ contracts/helloWord.sol
@@ -2,15 +2,15 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract Hello {
-    uint256 value;
-
-    constructor(uint256 _a) public {
-        value = _a;
-    }
-
-    function getValue() external view returns (uint256) {
-        return value;
-    }
+    uint256 public value;
+    //private
+    // internal 
+    // public 
+    // external 
+    
+    // function getValue() public view returns (uint256) {
+    //     return value;
+    // }
 
     function setValue(uint256 _value) external {
         value = _value;
